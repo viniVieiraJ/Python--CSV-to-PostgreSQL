657841872482bb8dd1f3e012a5ade0197c3af393
DamnVini <viniciusvdj25@gmail.com>
2022-07-16 20:26:13 -0300

    adicionada mais opções de manipulação
    + deletar por modelo
    + deletar por preço
    + update por preço

--- csv_to_db.py
+++ csv_to_db.py
@@ -70,13 +70,50 @@ class Moto(Connection):
             self.execute(sql, args)
             self.commit()
         except Exception as e:
-            print(" --> ERRO: nao foi possivel inserir informações no Banco de Dados", e) 
+            print(" --> ERRO: nao foi possivel inserir informações no Banco de Dados:", e) 
 
+    def delete_by_model(self, *args):
+        try:
+            sql = "DELETE FROM motos WHERE modelo = %s;"
+            self.execute(sql, args)
+            self.commit()
+        except Exception as e:
+            print(" --> ERRO: nao foi possivel deletar as informações no Banco de Dados:", e)
+    
+    def delete_by_price(self, *args):
+        try:
+            sql = "DELETE FROM motos WHERE preco = %s;"
+            self.execute(sql, args)
+            self.commit()
+        except Exception as e:
+            print(" --> ERRO: nao foi possivel deletar as informações no Banco de Dados:", e)
+
+    def update_preco(self, *args):
+        try:
+            sql = "UPDATE motos SET preco = %s WHERE modelo = %s"
+            self.execute(sql, args)
+            self.commit()
+        except Exception as e:
+            print(" --> ERRO: nao foi possivel atualizar as informações no Banco de Dados:", e)
 
 if __name__ == "__main__":
     moto = Moto()
 
     csv_file = pd.read_csv(sys.argv[1], sep=";")
+    operation = sys.argv[2] # delete_by_model, delete_by_price, update_preco, insert
 
-    for row in csv_file.values:
-        moto.insert(row[0], row[1])
+    if operation == "insert":
+        for row in csv_file.values:
+            moto.insert(row[0], row[1])
+
+    elif operation == "delete_by_model":
+        for row in csv_file["MODELO"]:
+            moto.delete_by_model(row)
+            
+    elif operation == "delete_by_price":
+        for row in csv_file["PRECO"]:
+            moto.delete_by_price(row)
+
+    elif operation == "update_preco":
+        for row in csv_file.values:
+            moto.update_preco(row[1], row[0])
